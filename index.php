64b03704a27c7fc362bff95e6f459cec6ffeb047
ipanracing <irfanbjm60@gmail.com>
2024-11-10 00:11:20 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,61 +1,70 @@
-<main>
-    <div class="container-fluid">
-        <h1 class="mt-4"></h1>
-        <ol class="breadcrumb mb-4">
-            <li class="breadcrumb-item"><a href="<?php echo site_url('user') ?>">User</a></li>
-            <li class="breadcrumb-item active"><?php echo $title ?></li>
-        </ol>
-        <div class="card mb-4">
-            <div class="card-header">
-                <a href="<?php echo site_url('user/add') ?>"><i class="fas fa-plus"></i>Add New</a>
-            </div>
-            <?php if ($this->session->flashdata('success')): ?>
-            <div class="alert alert-success" role="alert">
-                <?php echo $this->session->flashdata('success'); ?>
-            </div>
-            <?php endif; ?>
-            <div class="card-body">
-                <table class="table-responsive">
-                    <table class="table table-striped table-bordered table-hover" id="tabelkelas" width="100%" cellspacing="0"></table>
-                <thead>
-                    <tr>
-                        <th>No.</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                        <th>Role</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $no =1;
-                    foreach ($user as $user) {
-                        echo "<tr>"
-                        <td>$no</td>
-                        <td>$user->username</td>
-                        <td>$user->email</td>
-                        <td>$user->phone</td>
-
-                        <td>$user->role</td>
-                        <td>
-                        <div>
-                        <a href=".base_url('user/getedit/'. $user->id). class='btn btn-sm btn-info'><i class='fas fa-edit'></i> Edit</a?
-                        <a href=".base_url('user/delete/' .$user->id)."class='btn btn-sm btn-danger'
-                        onclick='return confirm(\"Ingin menghapus data user ini?\");'><i class='fas fa-trash'></i> Hapus</a>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Login - Disposisi Surat</title>
+        <link href="<?= base_url('assets/css/styles.css')?>" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+                                    <div class="card-body">
+                                        <?php echo $this->session->flashdata('message'); ?>
+                                        <form actiom="<?= base_url('login/dologin') ?>" method="POST">
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
+                                                <label for="inputEmail">Email address</label>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputPassword" type="password" placeholder="Password" />
+                                                <label for="inputPassword">Password</label>
+                                            </div>
+                                            <div class="form-check mb-3">
+                                                <input class="form-check-input" id="inputRememberPassword" type="checkbox" value="" />
+                                                <label class="form-check-label" for="inputRememberPassword">Remember Password</label>
+                                            </div>
+                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                <a class="small" href="password.html">Forgot Password?</a>
+                                                <input type="submit" class="btn btn-primary" value="Login" />
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="card-footer text-center py-3">
+                                        <div class="small"><a href="register.html">Need an account? Sign up!</a></div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        </td>
-                        </tr>";
-                        $no++;
-                    } ?>
-
-                    </tbody>
-                </table>
-                </div>
-                
                     </div>
+                </main>
             </div>
-        </div style="height: 100vh"></main></div>
-                </div>
-            </main>
-    </div>
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="<?= base_url('assets/js/script.js')?> "></script>
+    </body>
+</html>
